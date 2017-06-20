59c7b201039f4cecaff668622969022935d1aa5c
anibha88 <anibha88@gmail.com>
2017-06-21 02:38:40 +0530

    First commit with all dummy texts and working navigation links

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def mba
+  end
+
+  def bcom
+  end
+
+  def pu
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+
+  get '/about', to: 'pages#about', as: 'about'
+  get '/contact', to: 'pages#contact', as: 'contact'
+  get '/mba', to: 'pages#mba', as: 'mba'
+  get '/bcom', to: 'pages#bcom', as: 'bcom'
+  get '/pu', to: 'pages#pu', as: 'pu'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
