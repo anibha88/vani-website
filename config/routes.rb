c36001b862624c4c68ccd2d24e1748bf48e08214
anibha88 <anibha88@gmail.com>
2017-06-21 13:45:51 +0530

    testing contact form

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get '/about', to: 'pages#about', as: 'about'
-  get '/contact', to: 'pages#contact', as: 'contact'
+  # get '/contact', to: 'pages#contact', as: 'contact'
   get '/mba', to: 'pages#mba', as: 'mba'
   get '/bcom', to: 'pages#bcom', as: 'bcom'
   get '/pu', to: 'pages#pu', as: 'pu'
+
+  resources :contacts, only: [:new, :create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
