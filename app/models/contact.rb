6f985e9f4cb69d1606003ba598b4150820587aef
anibha88 <anibha88@gmail.com>
2017-06-22 18:06:29 +0530

    Type of course

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,6 +3,7 @@ class Contact < MailForm::Base
   attribute :telephone, :validate => /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message,   :validate => true
+  attribute :type,      :validate => true
   attribute :nickname,  :captcha  => true
 
   def headers
